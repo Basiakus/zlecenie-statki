40d40d5bf22c927f3a27c73a47f50bde524657bf
Adam Basiak <basiakus@gmail.com>
2018-09-16 21:28:40 +0200

    add hiding navigation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
+/* --NAVBUTTON IN SECTION#ONE-- */
 
-let slideIndex = 1; //current slide 
-const prevSlide = document.querySelector('.prev'); //prev slide button
-const nextSlide = document.querySelector('.next'); //next slide button
+const nav = document.querySelector('nav');
+const button = document.querySelector('#navButton');
+const navWidth = nav.offsetWidth;
+function showNavigation() {
+	const emptyContents = document.querySelectorAll('.emptyContent');
+	emptyContents.forEach( emptyContent => (!emptyContent.classList.contains('hide')) ? emptyContent.classList.add('hide') : emptyContent.classList.remove('hide'));
+	(!nav.classList.contains('off')) ? nav.classList.add('off') : nav.classList.remove('off');
+	(!button.classList.contains('click')) ? button.classList.add('click') : button.classList.remove('click');
+}
+button.addEventListener('click', showNavigation);
+
+/* --CARUSEL IN SECTION#TWO-- */
+
+let slideIndex = 1; 										// current slide 
+const prevSlide = document.querySelector('.prev');  		// prev slide button
+const nextSlide = document.querySelector('.next');  		// next slide button
 
 showSlide(slideIndex);
 
@@ -11,20 +25,21 @@ function plusSlide() {
 function minusSlide() {
 	showSlide(slideIndex += -1);
 }
-
 function showSlide(n) {
 	let i;
-	const slides = document.querySelectorAll('.mySlides'); //nodelist of .mySliders
-	if(n > slides.length) {slideIndex = 1} // after last slide change to first slide 
-	if(n < 1) {slideIndex = slides.length} // opposide as upper
+	const slides = document.querySelectorAll('.mySlides');	// nodelist of .mySliders
+	if(n > slides.length) {slideIndex = 1} 					// after last slide change to first slide 
+	if(n < 1) {slideIndex = slides.length}					// opposide as upper
 
-	for (i = 0; i < slides.length; i++) { // set all slides display none
+	for (i = 0; i < slides.length; i++) { 					// set all slides display none
 		slides[i].style.display = "none"; 
 	}
 	slides[slideIndex-1].style.display = "flex"; 
 }
-
 prevSlide.addEventListener('click', minusSlide);
 nextSlide.addEventListener('click', plusSlide);
+
+
+
 
 
